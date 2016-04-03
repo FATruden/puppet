12012caa2822cb54a104701c09d944518be2dc6d
Aleksandr Rudenko <a.rudikk@gmail.com>
2016-04-03 15:02:37 +0300

    Add rsync.sh

---
+++ rsync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Example
+#
+# ./rsync.sh <username>@<ip_or_hostname>
+
+
+RSYNC=/bin/rsync
+USR_DST=$1
+
+$RSYNC -zrvc    auth.conf \
+                environments \
+                fileserver.conf \
+                hiera.yaml \
+                hieradata \
+                manifests \
+                modules \
+                puppet.conf \
+                $USR_DST:/etc/puppet/
